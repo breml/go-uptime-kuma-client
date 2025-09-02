197fa8256c7139cc61789bd42cc04a05e378b622
Lucas Bremgartner <lucas@bremis.ch>
2025-09-02 22:12:16 +0200

    Use syncEmit for getSettings/setSettings

--- settings.go
+++ settings.go
@@ -2,59 +2,18 @@ package kuma
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/maldikhan/go.socket.io/socket.io/v5/client/emit"
 )
 
 func (c *Client) GetSettings(ctx context.Context) (map[string]any, error) {
-	res := make(chan ackResponse)
-	defer close(res)
-
-	err := c.socketioClient.Emit("getSettings",
-		emit.WithAck(func(response ackResponse) {
-			res <- response
-		}),
-	)
+	resp, err := c.syncEmit(ctx, "getSettings")
 	if err != nil {
-		return nil, fmt.Errorf("getSettings: %v", err)
+		return nil, err
 	}
 
-	select {
-	case response := <-res:
-		if !response.OK {
-			return nil, fmt.Errorf("getSettings: %s", response.Msg)
-		}
-
-		return response.Data, nil
-	case <-ctx.Done():
-		return nil, fmt.Errorf("getSettings: %v", ctx.Err())
-	}
+	return resp.Data, nil
 }
 
 func (c *Client) SetSettings(ctx context.Context, settings map[string]any, password string) error {
-	res := make(chan ackResponse)
-	defer close(res)
-
-	err := c.socketioClient.Emit("setSettings",
-		settings,
-		password,
-		emit.WithAck(func(response ackResponse) {
-			res <- response
-		}),
-	)
-	if err != nil {
-		return fmt.Errorf("setSettings: %v", err)
-	}
-
-	select {
-	case response := <-res:
-		if !response.OK {
-			return fmt.Errorf("setSettings: %s", response.Msg)
-		}
-
-		return nil
-	case <-ctx.Done():
-		return fmt.Errorf("setSettings: %v", ctx.Err())
-	}
+	_, err := c.syncEmit(ctx, "setSettings", settings, password)
+	return err
 }
